61e9d4e0e0cba80ac0c540c9a2c3cc76a03e27b3
THE TECH GUY <65334711+jaydumisuni@users.noreply.github.com>
2026-07-26 10:06:10 +0200

    Restore TypeScript lint configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  env: {
+    es2022: true,
+    node: true,
+  },
+  ignorePatterns: ["dist/", "node_modules/", "python/"],
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
